33d41853089d191fc2c9f6c7fe9064192df245a0
DimaSidukov <dmitriy.sidukov@gmail.com>
2023-04-06 08:13:02 +0300

    layout for home finished

--- iosApp/iosApp/Views/HomeView.swift
+++ iosApp/iosApp/Views/HomeView.swift
@@ -46,7 +46,7 @@ struct HomeView: View {
                 Image("bg_night")
                     .resizable()
                     .scaledToFit()
-                bgColor.padding(EdgeInsets(top: -10.0, leading: 0, bottom: 0, trailing: 0))
+                bgColor.padding(.top, -10)
             }.edgesIgnoringSafeArea(.all)
             VStack(alignment: .leading) {
                 HStack(alignment: .center) {
@@ -54,7 +54,7 @@ struct HomeView: View {
                         Image(systemName: "person.circle.fill")
                             .resizable()
                             .frame(width: 30, height: 30)
-                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                            .padding(.leading, 20)
                             .foregroundColor(.white)
                     }
                     Spacer()
@@ -62,10 +62,10 @@ struct HomeView: View {
                         Image(systemName: "line.3.horizontal")
                             .resizable()
                             .frame(width: 26.5, height: 22.1)
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                            .padding(.trailing, 20)
                             .foregroundColor(.white)
                     }
-                }.padding(EdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
+                }.padding(.top, 30)
                 HStack(alignment: .center) {
                     VStack(alignment: .leading) {
                         Text("Pekin".uppercased())
@@ -76,7 +76,7 @@ struct HomeView: View {
                             .font(.system(size: 13))
                             .fontWeight(.regular)
                             .foregroundColor(.white)
-                      }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    }.padding(.leading, 20)
                     Spacer()
                     VStack(alignment: .trailing) {
                         Text("24°C")
@@ -87,8 +87,8 @@ struct HomeView: View {
                             .font(.system(size: 21))
                             .fontWeight(.regular)
                             .foregroundColor(.white)
-                    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
-                }.padding(EdgeInsets(top: 100, leading: 0, bottom: 0, trailing: 0))
+                    }.padding(.trailing, 20)
+                }.padding(EdgeInsets(top: 108, leading: 0, bottom: 90, trailing: 0))
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack(spacing: 0) {
                         ForEach(0...locations.count - 1, id: \.self) { idx in
@@ -103,40 +103,41 @@ struct HomeView: View {
                                     .font(.system(size: 19))
                                     .fontWeight(.semibold)
                                     .foregroundColor(.white)
-                                    .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
-                            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                            
+                                    .padding(.top, 24)
+                            }.padding(.horizontal, 20)
                         }
                     }
-                }
+                }.frame(width: .infinity, height: 220)
                 Text("Today")
                     .font(.system(size: 20))
                     .fontWeight(.medium)
                     .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 20))
+                Spacer()
                 ScrollView(.horizontal, showsIndicators: false) {
                     LazyHStack(spacing: 0) {
                         ForEach(0...23, id: \.self) { idx in
                             VStack(spacing: 0) {
                                 Text("30°C")
-                                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+                                    .padding(5)
                                     .background(Color.white)
                                     .clipShape(Capsule())
-                                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                                    .padding(.top, 10)
                                 let text = "\(idx):00"
                                 Text(text)
                                     .fontWeight(.semibold)
                                     .font(.system(size: 15))
                                     .foregroundColor(.white)
-                                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                                    .padding(.vertical, 10)
                             }
                             .frame(width: 80, height: 80)
                             .background(itemBgColor)
                             .cornerRadius(16)
-                            .padding(EdgeInsets(top: 0, leading: 14, bottom: 14, trailing: 14))
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 14, trailing: 14))
                         }
                     }
                 }
+                Spacer()
             }
         }
     }
